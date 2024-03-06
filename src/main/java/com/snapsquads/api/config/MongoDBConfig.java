8eccef0c21a021d09027648e653be9a9eece03ff
Carlos Santos <carlosrsantos2001@gmail.com>
2024-03-06 01:14:00 -0300

    feat: initial commit

---
+++ src/main/java/com/snapsquads/api/config/MongoDBConfig.java
@@ -0,0 +1,19 @@
+package com.snapsquads.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
+
+@Configuration
+public class MongoDBConfig {
+    @Bean
+    public MongoDatabaseFactory mongoConfigure(){
+        return new SimpleMongoClientDatabaseFactory("mongodb://localhost:27017/snapsquads");
+    }
+
+    public MongoTemplate mongoTemplate() {
+        return new MongoTemplate(mongoConfigure());
+    }
+}
